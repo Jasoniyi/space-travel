45530345898bd29c7ef3e8f3134e5794b80926c5
jasoniyi <josephomoniyitope@gmail.com>
2024-01-03 21:19:10 +0100

    ft-crew page

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -1,39 +1,38 @@
+"use client";
 import React from "react";
 import Logo from "../../../../public/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 import { Barlow } from "../../Fonts/Fonts";
+import { usePathname } from "next/navigation";
 
 type NavLinksShape = {
   title: string;
   link: string;
-  digit: string;
 };
 
 const NavLinks: NavLinksShape[] = [
   {
-    digit: "",
     title: "00 Home",
     link: "/",
   },
   {
-    digit: "",
     title: "01 destination",
     link: "/destination",
   },
   {
-    digit: "02",
-    title: "crew",
-    link: "/",
+    title: "02 crew",
+    link: "/crew",
   },
   {
-    digit: "03",
-    title: "technology",
+    title: "03 technology",
     link: "/",
   },
 ];
 
 const Menu = () => {
+  const pathname = usePathname();
+
   return (
     <div className={`${Barlow.className}`}>
       {/* Mobile View */}
@@ -41,20 +40,21 @@ const Menu = () => {
       {/* Desktop Menu */}
       <div className="absolute top-10">
         <div className="hidden md:flex w-screen items-center">
-          <div className="mr-16">
+          <div className="mx-16">
             <Image src={Logo} alt="logo" />
           </div>
           <div className=" border-gray-500 border-b-[1px] w-[31em]"></div>
           <div className="bg-blue-500 backdrop-blur-md bg-opacity-5 flex-1">
             <div className="flex items-center justify-center text-white py-6">
               {NavLinks.map((links, i) => (
-                <Link
-                  key={`links-${i}`}
-                  href={links.link}
-                  className="hover:underline hover:underline-offset-8 hover:text-red-500"
-                >
-                  <div className="flex px-8 space-x-2 ">
-                    <span className="font-bold">{links.digit}</span>
+                <Link key={`links-${i}`} href={links.link} passHref>
+                  <div
+                    className={`flex px-8 space-x-2 ${
+                      pathname === links.link
+                        ? "underline underline-offset-8"
+                        : ""
+                    }`}
+                  >
                     <span className="uppercase tracking-widest">
                       {links.title}
                     </span>
---
+++ src/app/crew/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+import React, { useState } from "react";
+import spaceMissionData from "@/data/data";
+import { Barlow } from "../Fonts/Fonts";
+
+const page = () => {
+  const [activeCard, setActiveCard] = useState<string>("commander");
+
+  const crewLinks = [
+    "commander",
+    "mission specialist",
+    "pilot",
+    "flight engineer",
+  ];
+
+  const handleLinkClick = (link: string) => {
+    setActiveCard(link);
+  };
+
+  return (
+    <div className="bg-crewBck lg:h-screen lg:pt-[11em]">
+      <div className="">
+        {spaceMissionData.crew
+          .filter((item) => item.role.toLowerCase() === activeCard)
+          .map((crew, i) => (
+            <div
+              className="grid grid-cols-1 lg:grid-cols-5 max-sm:flex max-sm:flex-col-reverse max-sm:items-center"
+              key={`crew-${i}`}
+            >
+              <div className="lg:col-span-3 place-content-center lg:pl-[11em] py-0 sm:py-0 px-6 sm:px-0">
+                <div className="flex flex-col space-y-8 pb-16 sm:py-0">
+                  <div
+                    className={`${Barlow.className} md:flex space-x-2 uppercase tracking-widest font-semibold lg:pt-0 sm:pt-[8em] text-2xl pl-8 lg:pl-0 hidden`}
+                  >
+                    <span className="text-gray-500">02</span>
+                    <span className="text-spaceWhite">Meet your crew</span>
+                  </div>
+                  <div className="flex flex-col items-center lg:items-start space-y-4 sm:pt-12">
+                    <span className="text-spaceBlue uppercase text-2xl tracking-wide">
+                      {crew.role}
+                    </span>
+                    <span className="uppercase text-center lg:text-left text-5xl text-spaceWhite tracking-widest">
+                      {crew.name}
+                    </span>
+                    <p className="text-spaceBlue text-lg sm:w-[25em] text-center lg:text-left lg:items-start lg:w-[21em]">
+                      {crew.bio}
+                    </p>
+                  </div>
+                  <div className="hidden pt-16 md:flex justify-center lg:justify-start space-x-4">
+                    {crewLinks.map((link, i) => (
+                      <div
+                        key={`crewlinks-${i}`}
+                        className={`cursor-pointer h-3 w-3 rounded-full ${
+                          link === activeCard ? "bg-white" : "bg-gray-600"
+                        }`}
+                        onClick={() => handleLinkClick(link)}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="block sm:hidden max-sm:my-8">
+                <div className="py-6 flex justify-center space-x-4">
+                  {crewLinks.map((link, i) => (
+                    <div
+                      key={`crewlinks-${i}`}
+                      className={`cursor-pointer h-3 w-3 rounded-full ${
+                        link === activeCard ? "bg-white" : "bg-gray-600"
+                      }`}
+                      onClick={() => handleLinkClick(link)}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="lg:col-span-2">
+                <div className="h-[23em] sm:h-[27em] lg:h-[37em] pt-10 lg:pt-0 flex justify-center">
+                  <img src={crew.images} alt={crew.name} className="h-full" />
+                </div>
+                <div className="block sm:hidden mx-8">
+                  <div className="border-[0.001em] border-gray-400" />
+                </div>
+              </div>
+              <div className="block sm:hidden pt-[6em]">
+                <div
+                  className={`${Barlow.className} md:flex space-x-2 uppercase tracking-widest font-semibold`}
+                >
+                  <span className="text-gray-500">02</span>
+                  <span className="text-spaceWhite">Meet your crew</span>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default page;
